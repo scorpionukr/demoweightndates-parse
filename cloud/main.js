515ae67e488ddae9b47e22c01505375467ac395f
Scorpion <scorpionukr@users.noreply.github.com>
2017-01-05 12:09:00 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,37 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+//Parse.Cloud.define('hello', function(req, res) {
+//  res.success('Hi');
+//});
+
+var Parse = require('parse-cloud-express').Parse;
+
+
+Parse.Cloud.define("hello", function(request, response) {
+  console.log('Ran cloud function.');
+  // As with Parse-hosted Cloud Code, the user is available at: request.user
+  // You can get the users session token with: request.user.getSessionToken()
+  // Use the session token to run other Parse Query methods as that user, because
+  //   the concept of a 'current' user does not fit in a Node environment.
+  //   i.e.  query.find({ sessionToken: request.user.getSessionToken() })...
+  response.success("Hello world! " + (request.params.a + request.params.b));
+});
+
+
+Parse.Cloud.beforeSave('TestObject', function(request, response) {
+  console.log('Ran beforeSave on objectId: ' + request.object.id);
+  // if update the request object, we need to send it back with the response
+  response.success(request.object);
+});
+
+Parse.Cloud.afterSave('TestObject', function(request, response) {
+  console.log('Ran afterSave on objectId: ' + request.object.id);
+});
+
+Parse.Cloud.beforeDelete('TestObject', function(request, response) {
+  console.log('Ran beforeDelete on objectId: ' + request.object.id);
+  response.success();
+});
+
+Parse.Cloud.afterDelete('TestObject', function(request, response) {
+  console.log('Ran afterDelete on objectId: ' + request.object.id);
 });
